b2b213d7535e621f114a29e55540f801ea9bd52c
maulen <example@example.com>
2024-03-17 15:34:08 +0500

    first commit

---
+++ src/main/java/ru/job4j/todo/TodoApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
+
+}
