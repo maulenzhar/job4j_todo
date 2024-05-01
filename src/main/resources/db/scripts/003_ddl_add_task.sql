c429f58f62da87c8774ec17b797bb9517f262b1b
Maulen Zharylgasynov <exmpl@gmail.com>
2024-05-01 16:53:55 +0500

    ToOne

--- src/main/resources/db/scripts/003_ddl_add_task.sql
+++ src/main/resources/db/scripts/003_ddl_add_task.sql
@@ -1,8 +1,5 @@
 ALTER TABLE tasks ADD COLUMN user_id INT NOT NULL, ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id);
 insert into users (id, login, name, password) values (1, 'test', 'test', 'test');
-insert into tasks(title, description, created, done, user_id)
-values ('продукты', 'взять продукты', '2024-03-01 21:50:19.861300', false, 1) ON CONFLICT DO NOTHING;
-insert into tasks(title, description, created, done, user_id)
-values ('уборка', 'убраться в квартире', '2024-03-01 21:51:19.861300', false, 1) ON CONFLICT DO NOTHING;
-insert into tasks(title, description, created, done, user_id)
-values ('стирка', 'постирать вещи', '2024-03-01 21:52:19.861300', false, 1) ON CONFLICT DO NOTHING;
+insert into tasks(title, description, created, done, user_id) values ('продукты', 'взять продукты', '2024-03-01 21:50:19.861300', false, 1) ON CONFLICT DO NOTHING;
+insert into tasks(title, description, created, done, user_id) values ('уборка', 'убраться в квартире', '2024-03-01 21:51:19.861300', false, 1) ON CONFLICT DO NOTHING;
+insert into tasks(title, description, created, done, user_id) values ('стирка', 'постирать вещи', '2024-03-01 21:52:19.861300', false, 1) ON CONFLICT DO NOTHING;
